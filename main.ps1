76c5b8d39482ac70326506f370e4091f771ca669
atari-monk <atari.monk1@gmail.com>
2025-08-31 18:28:02 +0200

    Update content

--- main.ps1
+++ main.ps1
@@ -1,6 +1,10 @@
 . ("C:\Atari-Monk-Art\prompting\Prompt.ps1")
 . ("C:\Atari-Monk-Art\prompting\helpers.ps1")
 
-$loadedPrompt = Import-PromptFromFile -FilePath "C:\Atari-Monk-Art\prompting\turbo-laps-dev\scene-factory.json"
+$folderPath = "C:\Atari-Monk-Art\prompting\turbo-laps-dev"
+$fileName = "prompt"
+$fileType = ".json"
+$fullPath = $folderPath + "\" + $fileName  + $fileType
 
+$loadedPrompt = Import-PromptFromFile -FilePath $fullPath
 $loadedPrompt.Execute()
